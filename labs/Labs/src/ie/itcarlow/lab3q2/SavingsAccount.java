778a99cc7f1cd74e6f845bf84ddfd68e4e92e3f0
ciano <ciano@10.40.5.57>
2021-10-19 11:58:44 +0100

    Added new static int to be 1

--- labs/Labs/src/ie/itcarlow/lab3q2/SavingsAccount.java
+++ labs/Labs/src/ie/itcarlow/lab3q2/SavingsAccount.java
@@ -1,5 +1,20 @@
 package ie.itcarlow.lab3q2;
 
 public class SavingsAccount {
+	
+	private int accountNumber;
+	private static float annualInterestRate;
+	private float savingsBalance;
+	
+	private static int nextFreeAccountNumber;
+	
+	public SavingsAccount(int newAccountNumber) {
+		accountNumber = newAccountNumber;
+	}
+	
+	public int getAccountNumber( ) {
+		return accountNumber;
+	}
+
 
 }
