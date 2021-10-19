6aad29358aada130aec4eaa319612ed71061505e
ciano <ciano@10.40.5.57>
2021-10-19 11:36:23 +0100

    Correction for loop switch to while loop for tick the time

--- labs/Labs/src/ie/carlow/lab3q1/Clock.java
+++ labs/Labs/src/ie/carlow/lab3q1/Clock.java
@@ -7,14 +7,21 @@ public class Clock {
 	public static void main(String[] args) {
 		
 		Calendar cal = Calendar.getInstance();
-		Time t = new Time(cal.get((Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE))
+		Time t = new Time(cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE));
+		
+		long current = System.currentTimeMillis();
 		
 				
-				
-				
+		for(int i=0; i < 60; i++);{
+		long current2 = System.currentTimeMillis();
+														//Tick the time 
 		t.tick();
 		System.out.println(t.toString());
+		current = current;
 		
+		
+		
+		}
 	
 
 
