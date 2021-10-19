1f468add04cdc6c26618395d431a0c6ae8f46ab7
ciano <ciano@10.40.5.57>
2021-10-19 11:28:13 +0100

    Tick the time

--- labs/Labs/src/ie/carlow/lab3q1/Clock.java
+++ labs/Labs/src/ie/carlow/lab3q1/Clock.java
@@ -1,9 +1,22 @@
 package ie.carlow.lab3q1;
 
+import java.util.Calendar;
+
 public class Clock {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Calendar cal = Calendar.getInstance();
+		Time t = new Time(cal.get((Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE))
+		
+				
+				
+				
+		t.tick();
+		System.out.println(t.toString());
+		
+	
+
 
 	}
 
